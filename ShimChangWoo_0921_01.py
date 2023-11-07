b6e22b1d6b63801b3abd042909350d4c3e28817f
JJangWoo0418 <150120175+JJangWoo0418@users.noreply.github.com>
2023-11-07 17:24:22 +0900

    hihi

---
+++ ShimChangWoo_0921_01.py
@@ -0,0 +1,16 @@
+# 제목: 자연수의 2진수 변환
+# 작성자: 컴퓨터공학부 심창우
+# 작성일: 2023.09.21
+
+import math
+
+def binary_digits(n) :
+    count = 1
+    while n > 1 :
+        count = count + 1
+        n = n // 2
+    return count
+
+n = int(input("자연수 n을 입력하세요: "))
+print(f"총 비트수 ({n}) = {binary_digits(n)}")
+print(f"log2({n})은 {math.log2(n):.4f}")
---
+++ ShimChangWoo_0921_02.py
@@ -0,0 +1,27 @@
+# 제목: 팩토리얼 계산 문제
+# 작성자: 컴퓨터공학부 심창우
+# 작성일: 2023.09.21
+
+count_r = 0
+count_i = 0
+
+def factorial(n) :
+    global count_r
+    count_r += 1
+    if n == 0 :
+        return 1
+    else :
+        return n * factorial(n - 1)
+    
+def factorial_iter(n) :
+    global count_i
+    count_i += 1
+    result = 1
+    for k in range(n, 0, -1) :
+        result = result * k
+    return result
+
+n = int(input("정수 n을 입력하세요:"))
+
+print(f"순환 Factorial({n})={factorial(n)} 호출 횟수: {count_r}")
+print(f"반복 Factorial({n})={factorial_iter(n)} 호출 횟수: {count_i}")
